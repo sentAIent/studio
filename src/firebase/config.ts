4dbfa1c8445cf841507d488e7c1ab98bffc33ac4
Brian Leonard <bleonardca@gmail.com>
2025-10-16 20:30:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9408376505-2d97b",
+  "appId": "1:644381938227:web:b70f22dc3fd39451108946",
+  "apiKey": "AIzaSyBxrTkrp82FsUfCCogxk9G-Qlyd3NFd5pU",
+  "authDomain": "studio-9408376505-2d97b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "644381938227"
+};
